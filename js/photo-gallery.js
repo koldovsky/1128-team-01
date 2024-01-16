4314ddb87833a4ae84d530250f7dc6645fb5eaec
YaraBlack <yara26072000@gmail.com>
2024-01-17 00:04:15 +0200

    remove comments

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,14 +1,10 @@
 function openTab(evt, tabName) {
-  
-    // Get all elements with class "gallery__tab-content" and hide them
     const tab_content = document.querySelectorAll(".gallery__tab-content");
     tab_content.forEach(tab => tab.style.display = "none");
   
-    // Get all elements with class "gallery__tab-item--link" and remove the class "tab-item-active"
     const tab_links = document.querySelectorAll(".gallery__tab-item--link");
     tab_links.forEach(link => link.classList.remove("tab-item-active"))
   
-    // Show the current tab, and add a "tab-item-active" class to the button that opened the tab
     const currentTab = document.querySelector(`.${tabName}`)
     if (currentTab) {
       currentTab.style.display = "grid";
