96348e3c2227e2294fb7d90c2da3211c697d0b7c
YaraBlack <yara26072000@gmail.com>
2024-01-17 20:07:30 +0200

    refactor js

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,14 +1,20 @@
-function openTab(evt, tabName) {
-    const tab_content = document.querySelectorAll(".gallery__tab-content");
-    tab_content.forEach(tab => tab.style.display = "none");
+function openTab(event) {
+  const tabName = event.currentTarget.dataset.tab;
+  const tab_content = document.querySelectorAll(".gallery__tab-content");
+  tab_content.forEach(tab => tab.style.display = "none");
   
-    const tab_links = document.querySelectorAll(".gallery__tab-item--link");
-    tab_links.forEach(link => link.classList.remove("tab-item-active"))
+  const tab_links = document.querySelectorAll(".gallery__tab-item--link");
+  tab_links.forEach(link => link.classList.remove("tab-item-active"));
   
-    const currentTab = document.querySelector(`.${tabName}`)
-    if (currentTab) {
-      currentTab.style.display = "grid";
-    }
+  const currentTab = document.querySelector(`.${tabName}`);
+  currentTab.style.display = "grid";
 
-    evt.currentTarget.className += " tab-item-active";
+  event.currentTarget.classList.add("tab-item-active");
 }
+
+document.addEventListener("partialsLoaded", function() {
+  const tab_nav_btns = document.querySelectorAll(".gallery__tab-item--link");
+  tab_nav_btns.forEach(btn => {
+    btn.addEventListener('click', openTab);
+  });
+})
