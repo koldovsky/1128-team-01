5a03e693030b186d488f90b34419cfc1e9130f4a
YaraBlack <yara26072000@gmail.com>
2024-01-23 19:55:33 +0200

    fix links

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,34 +1,34 @@
-// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/javascript
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
-// https://www.codewars.com/kata/55685cd7ad70877c23000102/javascript
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
 function makeNegative(num) {
   return num > 0 ? -num : num;
 }
 
-// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/javascript
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
 function move(position, roll) {
   return position + roll * 2;
 }
 
-// https://www.codewars.com/kata/5772da22b89313a4d50012f7/javascript
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
 function greet(name, owner) {
   return name === owner ? "Hello boss" : "Hello guest";
 }
 
-// https://www.codewars.com/kata/582cb0224e56e068d800003c/javascript
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
 function litres(time) {
   return Math.floor(time * 0.5);
 }
 
-// https://www.codewars.com/kata/5772da22b89313a4d50012f7/javascript
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 
 function lovefunc(flower1, flower2) {
   return flower1 % 2 !== flower2 % 2;
