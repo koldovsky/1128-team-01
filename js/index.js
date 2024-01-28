4d5dbc6135fe2490310d6b3ac3c02c800c14a36b
Chelakhovl <147933854+Chelakhovl@users.noreply.github.com>
2024-01-28 15:10:25 +0000

    added question-animation.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 document.addEventListener('partialsLoaded', () => {
     import('./activity-list.js');
+    import('./question-animation.js');
 });
---
+++ js/question-animation.js
@@ -0,0 +1,18 @@
+var callUsButton = document.querySelector(".questions__call-us");
+var reservationButton = document.querySelector(".questions__reservation");
+
+callUsButton.addEventListener("mouseover", function () {
+  callUsButton.classList.add("animate-hover");
+});
+
+callUsButton.addEventListener("mouseout", function () {
+  callUsButton.classList.remove("animate-hover");
+});
+
+reservationButton.addEventListener("mouseover", function () {
+  reservationButton.classList.add("animate-hover");
+});
+
+reservationButton.addEventListener("mouseout", function () {
+  reservationButton.classList.remove("animate-hover");
+});
