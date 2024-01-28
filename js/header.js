2f5e22a017a67cd212680613ab102afd3f252c0f
Evhen Glotov <evegney.glotov@gmail.com>
2024-01-28 22:11:52 +0200

    modified:   css/footer.css 	modified:   css/header.css 	modified:   header.partial.html 	new file:   js/header.js 	modified:   js/index.js

---
+++ js/header.js
@@ -0,0 +1,9 @@
+const header = document.querySelector(".header");
+const btnSub = document.querySelector(".header__menu-btn");
+
+
+btnSub.addEventListener("click", () => {
+    body.classList.toggle("fixed");
+    header.classList.toggle("responsive");
+})
+
--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ document.addEventListener('partialsLoaded', () => {
     import('./activity-list.js');
     import('./question-animation.js');
     import('./shop-and-cafe-counter-clients.js');
+    import('./header.js')
 });
