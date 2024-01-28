725610f5eb23291f9a0b020c19b9a474cbe0d544
Chelakhovl <147933854+Chelakhovl@users.noreply.github.com>
2024-01-28 15:52:37 +0000

    created shop-and-cafe-counter-clients.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener('partialsLoaded', () => {
     import('./activity-list.js');
     import('./question-animation.js');
+    import('./shop-and-cafe-counter-clients.js');
 });
