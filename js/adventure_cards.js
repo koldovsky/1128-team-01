a144eb85218474eecc4f82413c787593d157108c
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2024-01-29 22:41:56 +0200

    Update adventure_cards.js

--- js/adventure_cards.js
+++ js/adventure_cards.js
@@ -1,41 +1,6 @@
-const adventureCards = [
-    {
-        id: 1,
-        src: "img/adventures/getaway.webp",
-        alt: "Yosemite Gateway Horse Ranch",
-        description: 'Yosemite Gateway Horse Ranch "Getaway" Cottage',
-    },
-    {
-        id: 2,
-        src: "img/adventures/toulumne.webp",
-        alt: "Tuolumne Meadows Lodge",
-        description: 'Tuolumne Meadows Lodge',
-    },
-    {
-        id: 3,
-        src: "img/adventures/sierra.webp",
-        alt: "Sierra Sunrise Vacation Rentale",
-        description: 'Sierra Sunrise Vacation Rentale',
-    },
-    {
-        id: 4,
-        src: "img/adventures/evergreen.webp",
-        alt: "Donya Marie's Cottage on Evergreen",
-        description: "Donya Marie's Cottage on Evergreen",
-    },
-    {
-        id: 5,
-        src: "img/adventures/sierra-cottage.webp",
-        alt: "Sierra Meadow Cottage",
-        description: "Sierra Meadow Cottage",
-    },
-    {
-        id: 6,
-        src: "img/adventures/river-house.webp",
-        alt: "Yosemite’s River House",
-        description: "Yosemite’s River House",
-    },
-]
+const response = await fetch('json/adventure_cards.json');
+const adventureCards = await response.json();
+renderAdventureCards(adventureCards);
 
 function renderAdventureCards(adventureCards) {
     let adventureCardsHtml = '';
@@ -56,9 +21,5 @@ function renderAdventureCards(adventureCards) {
             </div>
         </div>`;
     }
-    console.log(adventureCardsHtml)
     document.querySelector('.adventure__grid').innerHTML = adventureCardsHtml;
 }
-
-renderAdventureCards(adventureCards);
-  
