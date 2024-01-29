4fa7782909589d505347fbf821ca5da14903637b
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2024-01-29 22:37:21 +0200

    Separated adventures data to odject in js

---
+++ js/adventure_cards.js
@@ -0,0 +1,64 @@
+const adventureCards = [
+    {
+        id: 1,
+        src: "img/adventures/getaway.webp",
+        alt: "Yosemite Gateway Horse Ranch",
+        description: 'Yosemite Gateway Horse Ranch "Getaway" Cottage',
+    },
+    {
+        id: 2,
+        src: "img/adventures/toulumne.webp",
+        alt: "Tuolumne Meadows Lodge",
+        description: 'Tuolumne Meadows Lodge',
+    },
+    {
+        id: 3,
+        src: "img/adventures/sierra.webp",
+        alt: "Sierra Sunrise Vacation Rentale",
+        description: 'Sierra Sunrise Vacation Rentale',
+    },
+    {
+        id: 4,
+        src: "img/adventures/evergreen.webp",
+        alt: "Donya Marie's Cottage on Evergreen",
+        description: "Donya Marie's Cottage on Evergreen",
+    },
+    {
+        id: 5,
+        src: "img/adventures/sierra-cottage.webp",
+        alt: "Sierra Meadow Cottage",
+        description: "Sierra Meadow Cottage",
+    },
+    {
+        id: 6,
+        src: "img/adventures/river-house.webp",
+        alt: "Yosemite’s River House",
+        description: "Yosemite’s River House",
+    },
+]
+
+function renderAdventureCards(adventureCards) {
+    let adventureCardsHtml = '';
+    for (const card of adventureCards) {
+      adventureCardsHtml += `
+        <div class="adventure__grid-item grid-item">
+            <div class="item__image-wrapper image-wrapper">
+                <img
+                    class="image-wrapper-img"
+                    src="${card.src}"
+                    alt="${card.alt}"/>
+                <button class="image-wrapper-btn-lightbox"></button>
+            </div>
+            <div class="grid-item__text-wrapper">
+                <h3 class="grid-item__item-text">
+                    ${card.description}
+                </h3>
+            </div>
+        </div>`;
+    }
+    console.log(adventureCardsHtml)
+    document.querySelector('.adventure__grid').innerHTML = adventureCardsHtml;
+}
+
+renderAdventureCards(adventureCards);
+  
--- js/index.js
+++ js/index.js
@@ -3,5 +3,6 @@ document.addEventListener('partialsLoaded', () => {
     import('./question-animation.js');
     import('./shop-and-cafe-counter-clients.js');
     import('./header.js');
+    import('./adventure_cards.js')
     import('./lightbox.js');
 });
