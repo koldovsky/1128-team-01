42121951964614e325dd7f768bc79c69e761cc39
Chelakhovl <147933854+Chelakhovl@users.noreply.github.com>
2024-01-30 15:05:08 +0000

    changed shop-and-cafe-counter-clients.js

--- js/shop-and-cafe-counter-clients.js
+++ js/shop-and-cafe-counter-clients.js
@@ -1,19 +1,12 @@
 const clientsCount = document.querySelector(".clients__count");
+const maxClientsIncrement = 6;
+
 clientsCount.textContent = 1024;
 
+const changeClientsCount = () => {
+  clientsCount.textContent = Number(clientsCount.textContent) + getRandomInt(maxClientsIncrement);
+};
+
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
 setInterval(changeClientsCount, 5000);
-
-function changeClientsCount() {
-  clientsCount.textContent = setClientsCount(
-    getRandomInt(6),
-    parseInt(clientsCount.textContent, 10)
-  );
-}
-
-function setClientsCount(value, clientsCount) {
-  return (clientsCount += value);
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
