558920c3857233eb19a814c37cc04b4efc27e440
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2024-01-31 15:30:45 +0200

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,7 +1,7 @@
 function openModal(path) {
     const modal = document.getElementById("lightboxModal");
     const element = document.querySelector('.lightbox-modal__image');
-    modal.style.display = "block";
+    modal.style.display = "grid";
     element.src = path;
 }
 
@@ -10,7 +10,7 @@ function attachLightboxEventListeners() {
     closeButton.addEventListener("click", () => document.getElementById("lightboxModal").style.display = "none");
    
     const lightboxButtons = document.getElementsByClassName("image-wrapper-btn-lightbox");
-
+    
     Array.from(lightboxButtons).forEach(button => {
         button.addEventListener("click", () => {
             const imagePath = button.previousElementSibling.getAttribute("src");
