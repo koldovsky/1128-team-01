bb3f89a754428ca76702b068a56cd114f7629ef0
hrybvalerii <133959313+hrybvalerii@users.noreply.github.com>
2024-02-05 09:49:28 +0000

    fixed modal window

--- js/how-to-get-contacts.js
+++ js/how-to-get-contacts.js
@@ -1,22 +1,30 @@
 const form = document.querySelector(".contact__form");
 const message = document.querySelector(".overlaybg")
-let phoneNum = document.querySelector(".phone-number");
-let userName = document.querySelector(".name");
+const phoneField = document.querySelector(".phone-number");
+const userNameField = document.querySelector(".name");
 const submitButton = document.querySelector(".contact__submit")
-const okButton = document.querySelector(".messagebox__button")
+const okButton = document.querySelector(".modal-window__button")
 
 const maxUsernameLength = 5
 
-form.addEventListener("submit", showMessageBox);
-okButton.addEventListener("click", hideMessageBox)
+form.addEventListener("submit", showModalWindow);
+okButton.addEventListener("click", hideModalWindow)
 
-function showMessageBox(event) {
+function showModalWindow(event) {
     event.preventDefault();
     message.style.display = "flex";
 }
 
-function hideMessageBox() {
+function hideModalWindow() {
     message.style.display = "none"
 }
 
+// function validatPhone {
 
+// }
+
+function validateName() {
+    if (userNameField.value.length > maxUsernameLength) {
+        console.log('too long name')
+    }
+}
