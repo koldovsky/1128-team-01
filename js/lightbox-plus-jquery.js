3cbe28d2a8c3a0b3c980884c91119ebcf825bed7
Дмитро <145404351+DMan113@users.noreply.github.com>
2024-02-07 14:58:27 +0000

    update

--- js/lightbox-plus-jquery.js
+++ js/lightbox-plus-jquery.js
@@ -11036,7 +11036,7 @@ return jQuery;
   // http://lokeshdhakar.com/projects/lightbox2/index.html#options
   Lightbox.defaults = {
     albumLabel: 'Image %1 of %2',
-    alwaysShowNavOnTouchDevices: false,
+    alwaysShowNavOnTouchDevices: true,
     fadeDuration: 600,
     fitImagesInViewport: true,
     imageFadeDuration: 600,
@@ -11044,9 +11044,9 @@ return jQuery;
     // maxHeight: 600,
     positionFromTop: 50,
     resizeDuration: 700,
-    showImageNumberLabel: true,
-    wrapAround: false,
-    disableScrolling: false,
+    showImageNumberLabel: false,
+    wrapAround: true,
+    disableScrolling: true,
     /*
     Sanitize Title
     If the caption data is trusted, for example you are hardcoding it in, then leave this to false.
